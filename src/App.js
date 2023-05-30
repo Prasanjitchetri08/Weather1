6a365e18196936349448031fbc3db6960fb986ab
Prasanjit chetri <prasanjitchetri08@gmail.com>
2023-05-30 13:28:18 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import WheatherApp from './component/WheatherApp';
+function App() {
+  return (
+ <>
+ <WheatherApp/>
+ </>
+  );
+}
+
+export default App;
